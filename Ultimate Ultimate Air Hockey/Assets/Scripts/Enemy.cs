c71c6aaa932dba9399951056dcf36c76c295be58
Jeffrey Chesanek <jchesanek513@g.rwu.edu>
2020-11-18 15:27:17 -0500

    Completely new project

---
+++ Ultimate Ultimate Air Hockey/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  public float MaxMovementSpeed;
+  private Rigidbody2D rb;
+  private Vector2 startingPosition;
+  public Rigidbody2D Puck;
+  public Transform thePuck;
+
+  private Vector2 targetPosition; 
+ // public Transform PlayerBoundaryHolder; 
+    private void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        //startingPosition = rb.position;
+        startingPosition = transform.position;
+        //GetComponent<Collider>().material.bounciness = 0f;
+
+    }
+
+    private void FixedUpdate(){
+        float movementSpeed;
+
+        movementSpeed = MaxMovementSpeed * Random.Range(0.1f, 0.3f);
+        //movementSpeed = Random.Range(MaxMovementSpeed * 0.4f, MaxMovementSpeed);
+        //rb.MovePosition(Vector2.MoveTowards(rb.position, targetPosition, movementSpeed * Time.fixedDeltaTime));
+
+        targetPosition.x = thePuck.position.x;
+        targetPosition.y = thePuck.position.y;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed * Time.fixedDeltaTime);
+        /*
+        if(transform.position.x == 5.40){
+            targetPosition.x = 3;
+            targetPosition.y = thePuck.position.y;
+            while(transform.position.x > 3){
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed * Time.fixedDeltaTime);
+            }
+        }*/
+    }
+}
---
+++ Ultimate Ultimate Air Hockey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public GameObject gameOverText;
+    public GameObject player;
+
+    public GameObject puck;
+    public Transform ThePuck;
+
+    public GameObject AI;
+    public Text PlayerScoreText;
+    public Text AIScoreText;
+    public GameObject EnemyGoalText;
+    public int PlayerScore = 0;
+    public int AIScore = 0;
+
+    public bool Goal = false;
+    //public GameObject AI; set this later
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+        gameOverText.SetActive(false);
+        EnemyGoalText.SetActive(false);
+        DisplayTime(timeRemaining);
+        PlayerScoreText.text = "Player: " + PlayerScore;
+        AIScoreText.text = "AI: " + AIScore;
+    }
+
+    void Update()
+    {
+        if(Goal){
+            Goal = false;
+            EnemyGoalText.SetActive(false);
+            Debug.Log("Here");
+            Vector2 puckPosition = ThePuck.position;
+            puckPosition.x = -7;
+            puckPosition.y = -1;
+        }
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                gameOverText.SetActive(true);
+                player.SetActive(false);
+                AI.SetActive(false);
+                puck.SetActive(false); //disable them, but if game goes to OT, then reactivate
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+        //if(GameObject.Find("puck").transform.position.x < -13.75){
+        Vector2 puckPos = ThePuck.position;
+        if(puckPos.x < -13.75){
+            //Debug.Log("yup");
+            puck.SetActive(false);
+            EnemyGoal();
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText = GameObject.Find("TimeText").GetComponent<Text>();
+        timeText.text = "Time: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void EnemyGoal(){
+        EnemyGoalText.SetActive(true);
+        Goal = true;
+    }
+ 
+    IEnumerator Delay(){ 
+        yield return new WaitForSeconds (4f);
+    }
+    
+
+}
